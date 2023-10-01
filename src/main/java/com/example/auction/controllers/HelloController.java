1c871082efc70b2a0e78afd34b66f54fa5c07066
MaksymKalennyk <107300165+MaksymKalennyk@users.noreply.github.com>
2023-10-01 21:56:14 +0300

    Adding Models: 1. Lot 2. StartingPrice
    Adding Repositories:
    1. LotRepository
    2. StartingPriceRepository

--- src/main/java/com/example/auction/controllers/HelloController.java
+++ src/main/java/com/example/auction/controllers/HelloController.java
@@ -1,4 +1,4 @@
-package controllers;
+package com.example.auction.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
