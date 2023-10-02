76df437ee1d9ef84ff03e996c1c72191fa7b3a2b
Qwertua <wertua17062004@gmail.com>
2023-10-02 21:49:25 +0300

    Adding method searchByTittle to LotsService

--- src/main/java/com/example/auction/controllers/HelloController.java
+++ src/main/java/com/example/auction/controllers/HelloController.java
@@ -1,13 +1,27 @@
 package com.example.auction.controllers;
 
+import com.example.auction.models.Lots;
+import com.example.auction.services.LotsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class HelloController {
-
+    private final LotsService lotsService;
+    @Autowired
+    public HelloController(LotsService lotsService) {
+        this.lotsService = lotsService;
+    }
     @GetMapping("hello")
     public String hello(){
         return "Hello World!";
+    }
+    @GetMapping("/search/{word}")
+    public List<Lots> searchByTittle(@PathVariable String word){
+         return lotsService.searchLotByTittle(word);
     }
 }
