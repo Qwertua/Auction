46864cba78be8897fa0a5995c6582751c3b563a7
MaksymKalennyk <107300165+MaksymKalennyk@users.noreply.github.com>
2023-10-02 19:44:32 +0300

    adding action: 1. Start 2. Stop

--- src/main/java/com/example/auction/services/LotsService.java
+++ src/main/java/com/example/auction/services/LotsService.java
@@ -6,40 +6,55 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
 public class LotsService {
 
     private final LotRepository lotRepository;
+    private final Map<Long, Boolean> auctionStatusMap = new HashMap<>();
 
     @Autowired
-    public LotsService(LotRepository lotRepository){
+    public LotsService(LotRepository lotRepository) {
         this.lotRepository = lotRepository;
     }
 
-    public Lots findById(Long id){
+    public Lots findById(Long id) {
         return lotRepository.findById(id).orElse(null);
     }
 
-    public Lots createLots(Lots lots){
+    public Lots createLots(Lots lots) {
         return lotRepository.save(lots);
     }
 
-    public void deleteByID(Long id){
+    public void deleteByID(Long id) {
         lotRepository.deleteById(id);
     }
 
-    public List<Lots> getAllLots(){
+    public List<Lots> getAllLots() {
         return lotRepository.findAll();
     }
 
-    public List<Lots> searchActiveLots(){
+    public List<Lots> searchActiveLots() {
         LocalDateTime currentTime = LocalDateTime.now();
         return getAllLots().stream()
-                .filter(lot -> currentTime.isAfter(lot.getStartTime()) && currentTime.isBefore(lot.getEndTime()))
+                .filter(lot -> currentTime.isAfter(lot.getStartTime()) && currentTime.isBefore(lot.getEndTime()) && isAuctionRunning(lot.getId()))
                 .collect(Collectors.toList());
+    }
+
+    public void startAuction(Long lotId) {
+        auctionStatusMap.put(lotId, true);
+    }
+
+    public void stopAuction(Long lotId) {
+        auctionStatusMap.put(lotId, false);
+    }
+
+    private boolean isAuctionRunning(Long lotId) {
+        return auctionStatusMap.getOrDefault(lotId, false);
     }
 }
 
