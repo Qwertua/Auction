357b47dbd31304f904413b51e5fc638ad6d7e0e6
MaksymKalennyk <107300165+MaksymKalennyk@users.noreply.github.com>
2023-10-05 19:16:00 +0300

    updating price in lotList

--- src/main/java/com/example/auction/services/StartingPriceService.java
+++ src/main/java/com/example/auction/services/StartingPriceService.java
@@ -1,32 +1,41 @@
 package com.example.auction.services;
 
+import com.example.auction.models.Lots;
 import com.example.auction.models.StartingPrice;
 import com.example.auction.repo.StartingPriceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.List;
 
 @Service
 public class StartingPriceService {
 
     private final StartingPriceRepository startingPriceRepository;
+    private final LotsService lotsService;
 
     @Autowired
-    public StartingPriceService(StartingPriceRepository startingPriceRepository) {
+    public StartingPriceService(StartingPriceRepository startingPriceRepository, LotsService lotsService) {
         this.startingPriceRepository = startingPriceRepository;
+        this.lotsService = lotsService;
     }
 
-    public StartingPrice updatePriceById(Long id, StartingPrice updatedPrice) {
+    public void updatePriceByLotTitle(String lotTitle, int newPrice) {
+        List<Lots> lotList = lotsService.searchLotByTittle(lotTitle);
 
-        Optional<StartingPrice> optionalStartingPrice = startingPriceRepository.findById(id);
-        StartingPrice existingPrice = optionalStartingPrice.get();
+        if (!lotList.isEmpty()) {
+            for (Lots lot : lotList) {
+                StartingPrice startingPrice = lot.getStartingPrice();
 
-        if (updatedPrice.getPrice() > existingPrice.getPrice()) {
-            existingPrice.setPrice(updatedPrice.getPrice());
-            return startingPriceRepository.save(existingPrice);
+                if (newPrice > startingPrice.getPrice()) {
+                    startingPrice.setPrice(newPrice);
+                    startingPriceRepository.save(startingPrice);
+                } else {
+                    throw new IllegalArgumentException("Updated price must be > existing price");
+                }
+            }
         } else {
-            throw new IllegalArgumentException("Updated price must be > existing price");
+            throw new IllegalArgumentException("Lot not found with title: " + lotTitle);
         }
     }
 
