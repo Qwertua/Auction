bc3f157b345686280e4a1f2f2d238e2e760feccc
MaksymKalennyk <107300165+MaksymKalennyk@users.noreply.github.com>
2023-10-23 17:57:54 +0300

    adding config + angular User: 1. model 2. repo 3. service 4. rest controller

---
+++ src/main/java/com/example/auction/rest/UserRestController.java
@@ -0,0 +1,36 @@
+package com.example.auction.rest;
+
+import com.example.auction.models.Users;
+import com.example.auction.services.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api")
+public class UserRestController {
+
+    private final UsersService usersService;
+
+    @Autowired
+    public UserRestController(UsersService usersService) {
+        this.usersService = usersService;
+    }
+
+    @GetMapping("/all/users")
+    public List<Users> getAllLots(){
+        return usersService.getAllUsers();
+    }
+
+    @GetMapping("/search/user/{id}")
+    public Users searchById(@PathVariable Long id){
+        return usersService.findById(id);
+    }
+
+    @PostMapping("/save/user")
+    public Users saveUser(Users users){
+        return usersService.saveUsers(users);
+    }
+}
